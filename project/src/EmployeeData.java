ab3d5ac866b02b6fc8223bace4fd77c013ae8c88
Anıl Elmaz <143184395+AE1024@users.noreply.github.com>
2025-02-23 16:29:09 +0300

    Delete project directory

--- project/src/EmployeeData.java
+++
@@ -1,6 +0,0 @@
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
-public class EmployeeData {
-    public static ObservableList<Employee> employeeList = FXCollections.observableArrayList();
-}
